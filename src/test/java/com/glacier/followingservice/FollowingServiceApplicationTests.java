84c679287dde2b7c622f8f4968c176643689c3b1
sshaparenko <zshaparenkoz@gmail.com>
2024-04-11 16:37:52 +0300

    initial commit

---
+++ src/test/java/com/glacier/followingservice/FollowingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glacier.followingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
