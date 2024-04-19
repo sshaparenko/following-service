f5d24f9893ccd10f7cd7f89726ecc4e55facac58
sshaparenko <zshaparenkoz@gmail.com>
2024-04-19 12:20:38 +0300

    Change concatenation to text block

--- src/main/java/com/glacier/followingservice/FollowSqlStatements.java
+++ src/main/java/com/glacier/followingservice/FollowSqlStatements.java
@@ -2,11 +2,12 @@ package com.glacier.followingservice;
 
 public enum FollowSqlStatements {
 
-    GET_FOLLOWERS("SELECT FOLLOWER.USER_ID FROM \n" +
-                  "FOLLOWS F, USERS FOLLOWER, USERS FOLLOWED \n" +
-                  "WHERE FOLLOWER.USER_ID = F.FOLLOWER_ID\n" +
-                  "AND FOLLOWED.USER_ID = F.FOLLOWED_ID\n" +
-                  "AND FOLLOWED.USER_NAME = ?"),
+    GET_FOLLOWERS("""
+            SELECT FOLLOWER.USER_ID FROM\s
+            FOLLOWS F, USERS FOLLOWER, USERS FOLLOWED\s
+            WHERE FOLLOWER.USER_ID = F.FOLLOWER_ID
+            AND FOLLOWED.USER_ID = F.FOLLOWED_ID
+            AND FOLLOWED.USER_NAME = ?"""),
     GET_USER_INFO_BY_ID("SELECT USER_ID, USER_NAME FROM USERS WHERE USER_ID = ?"),
     GET_USER_ID_BY_USER_NAME("SELECT USER_ID FROM USERS WHERE USER_NAME = ?"),
     USER_ID("user_id"), USER_NAME("user_name");
